5d3a60ff89798a3bb049a715b8496b241e5eaf2e
H0uss3m <hossem.benmustapha@gmail.com>
2021-02-12 00:08:06 +0100

    add row

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,9 @@ import React from 'react'
 import "./HomeScreen.css"
 import Nav from "./Nav"
 import Banner from "./Banner"
+import Row from "./Row"
+import requests from '../Request'
+
 function HomeScreen() {
     return (
         <div className="homeScreen">
@@ -10,7 +13,14 @@ function HomeScreen() {
 
             <Banner/>
 
-            {/* Row */}
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow/>
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
         </div>
     )
 }
