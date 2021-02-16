215aa0b51d88f4ff08517f24bfca26bc1506b75d
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-16 15:19:10 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import HomeScreen from './components/HomeScreen'
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
