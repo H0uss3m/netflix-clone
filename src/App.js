f9805edb221ab8553c37368de146c45fd77bbe53
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-16 15:27:22 +0100

    add login screen

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import HomeScreen from './components/HomeScreen'
+import Login from './components/Login'
 
 function App() {
+  const user = null;
   return (
     <div className="app">
 
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <Login />
+        ) : (
+            <Switch>
+              <Route exact path="/">
+                <HomeScreen />
+              </Route>
+            </Switch>
+          )}
+
       </Router>
 
     </div>
