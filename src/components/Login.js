508109d7b1e1a8f01f54e855a3c39e729bb57fcf
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-16 20:42:25 +0100

    add login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import SignIn from './SignIn'
 
 function Login() {
+    const { signIn, setSignIn } = useState(false);
     return (
-        <div className="login">
-            Login
+        <div className="loginScreen">
+            <div className="loginScreen_background">
+                <img
+                    className="loginScreen_logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="netflix-logo" />
+                <button
+                    className="loginScreen_button"
+                    onClick={() => setSignIn(true)}>
+                    Sign In
+                </button>
+                <div className="loginScreen_gradient"></div>
+            </div>
+            <div className="loginScreen_body">
+                {signIn ? (
+                    <SignIn />
+                ) : (
+                    <>
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <h2>Watch anywhere. Cancel at any time</h2>
+                    <h3>
+                        Ready to watch? Enter your email to
+                        create or restart your
+                        membership.
+                    </h3>
+                    <div className="loginScreen_input">
+                        <form>
+                            <input
+                                type="email"
+                                onClick={() => setSignIn(true)}
+                                placeholder="Email Adress" />
+                            <button className="loginScreen_getStarted">GET STARTED</button>
+                        </form>
+                    </div>
+                </>
+                )}
+               
+            </div>
         </div>
     )
 }
