1b2971512342b9982571e17b73df525be5853481
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-17 14:17:32 +0100

    set firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBfR9GLIPXOiJpWVcV2GH7wvPOVxgmi3eo",
-    authDomain: "netflix-customized.firebaseapp.com",
-    projectId: "netflix-customized",
-    storageBucket: "netflix-customized.appspot.com",
-    messagingSenderId: "225768714805",
-    appId: "1:225768714805:web:f1c9ac77d3f74edf44377c",
-    measurementId: "G-0KV957RRF6"
-  };
+  apiKey: "AIzaSyBfR9GLIPXOiJpWVcV2GH7wvPOVxgmi3eo",
+  authDomain: "netflix-customized.firebaseapp.com",
+  projectId: "netflix-customized",
+  storageBucket: "netflix-customized.appspot.com",
+  messagingSenderId: "225768714805",
+  appId: "1:225768714805:web:f1c9ac77d3f74edf44377c",
+  measurementId: "G-0KV957RRF6"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const awth = firebase.awth();
+
+export { awth }
+export default db;
