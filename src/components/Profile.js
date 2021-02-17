61d5ab0cef13b7cd4a68d314afd279799b410881
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-17 17:19:16 +0100

    fix s

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,6 +17,7 @@ function Profile() {
                     <div className="profileScreen_details">
                         <h2>{user.email}</h2>
                         <div className="profileScreen_plans">
+                            <h3>Plans</h3>
                             <button className="profileScreen_signOut" onClick={() => auth.signOut()}>Sign Out</button>
                         </div>
                     </div>
