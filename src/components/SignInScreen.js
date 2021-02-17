e0ba57d1f01b496b2ff4979a0143e57e7695e05d
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-17 14:16:47 +0100

    implement authentication

--- src/components/SignInScreen.js
+++ src/components/SignInScreen.js
@@ -2,16 +2,25 @@ import React from 'react'
 import './SignInScreen.css'
 
 function SignInScreen() {
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+
+    const signIn = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="SignInScreen">
             <form>
                 <h1>Sign In</h1>
                 <input type="email" placeholder="Email"/>
                 <input type="password" placeholder="Password"/>
-                <button type="submit">Sign In</button>
+                <button type="submit" onClick={signIn}>Sign In</button>
                 <h4>
                     <span className="SignInScreen_gray">New to Netflix?</span>
-                    <span className="SignInScreen_link">Sign Up now.</span>
+                    <span className="SignInScreen_link" onClick={register}>Sign Up now.</span>
                 </h4>
             </form>
         </div>
