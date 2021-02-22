4c137bd38857f4b962a82b128046ce9648342bfb
h0uss3m <hossem.benmustapha@gmail.com>
2021-02-22 17:29:49 +0100

    set login and register screens

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,76 @@
-import React, { useState } from 'react'
-import './Login.css'
-import SignInScreen from './SignInScreen'
+import React, { useState } from "react";
+import "./Login.css";
+import SignInScreen from "./SignInScreen";
 
 function Login() {
-    const [signIn, setSignIn] = useState(false);
-    return (
-        <div className="loginScreen">
-            <div className="loginScreen_background">
-                <img
-                    className="loginScreen_logo"
-                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-                    alt="netflix-logo" />
-                <button
-                    className="loginScreen_button"
-                    onClick={() => setSignIn(true)}>
-                    Sign In
-                </button>
-                <div className="loginScreen_gradient"></div>
-            </div>
-            <div className="loginScreen_body">
-                {signIn ? (
-                    <SignInScreen />
-                ) : (
-                        <>
-                            <h1>Unlimited films, TV programmes and more.</h1>
-                            <h2>Watch anywhere. Cancel at any time</h2>
-                            <h3>
-                                Ready to watch? Enter your email to
-                                create or restart your
-                                membership.
-                    </h3>
-                            <div className="loginScreen_input">
-                                <form>
-                                    <input
-                                        type="email"
-                                        placeholder="Email Adress" />
-                                    <button
-                                        className="loginScreen_getStarted"
-                                        onClick={() => setSignIn(true)}>
-                                        GET STARTED
-                                    </button>
-                                </form>
-                            </div>
-                        </>
-                    )}
+  const [signIn, setSignIn] = useState(false);
+  const [register, setRegister] = useState(false);
+  const [email, setEmail] = useState("");
 
+  const onRegister = () => {
+    setRegister(true);
+    setSignIn(false);
+  };
+  
+  const OnSignIn = () => {
+    setRegister(false);
+    setSignIn(true);
+  };
+
+  const goToHome = () => {
+    setRegister(false);
+    setSignIn(false);
+    setEmail("");
+  };
+
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen_background">
+        <img
+          className="loginScreen_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="netflix-logo"
+          onClick={() => goToHome()}
+        />
+        <button className="loginScreen_button" onClick={() => OnSignIn()}>
+          Sign In
+        </button>
+        <button className="register_button" onClick={() => onRegister()}>
+          Register
+        </button>
+        <div className="loginScreen_gradient"></div>
+      </div>
+      <div className="loginScreen_body">
+        {signIn || register ? (
+          <SignInScreen email={email} register={register} signIn={signIn} />
+        ) : (
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="loginScreen_input">
+              <form>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="Email Adress"
+                />
+                <button
+                  className="loginScreen_getStarted"
+                  onClick={() => setSignIn(true)}
+                >
+                  GET STARTED
+                </button>
+              </form>
             </div>
-        </div>
-    )
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/SignInScreen.js
+++ src/components/SignInScreen.js
@@ -1,45 +1,83 @@
-import React, { useRef } from 'react'
-import { auth } from '../firebase'
-import './SignInScreen.css'
+import React, { useRef } from "react";
+import { auth } from "../firebase";
+import "./SignInScreen.css";
 
-function SignInScreen() {
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
+const SignInScreen = ({ register, signIn, email }) => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const registerEmailRef = useRef(null);
+  const registerPasswordRef = useRef(null);
 
-    const register = (e) => {
-        e.preventDefault();
-        auth.createUserWithEmailAndPassword(
-            emailRef.current.value,
-            passwordRef.current.value
-        ).then((authUser) => {
-            console.log('authUser', authUser)
-        }).catch(error => alert(error.message))
-    }
+  const onRegister = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        registerEmailRef.current.value,
+        registerPasswordRef.current.value
+      )
+      .then((authUser) => {
+        console.log("authUser", authUser);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const signIn = (e) => {
-        e.preventDefault()
-        auth.signInWithEmailAndPassword(
-            emailRef.current.value,
-            passwordRef.current.value
-        ).then((authUser) => {
-            console.log(authUser)
-        }).catch(error => alert(error.message))
-    }
+  const onSignIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    return (
-        <div className="SignInScreen">
-            <form>
-                <h1>Sign In</h1>
-                <input ref={emailRef} type="email" placeholder="Email" />
-                <input ref={passwordRef} type="password" placeholder="Password" />
-                <button type="submit" onClick={signIn}>Sign In</button>
-                <h4>
-                    <span className="SignInScreen_gray">New to Netflix?</span>
-                    <span className="SignInScreen_link" onClick={register}>Sign Up now.</span>
-                </h4>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="SignInScreen">
+      <form>
+        {signIn && !register ? (
+          <>
+            <h1>Sign In</h1>
+            <input
+              defaultValue={email}
+              ref={emailRef}
+              type="email"
+              placeholder="Email"
+            />
+            <input ref={passwordRef} type="password" placeholder="Password" />
+            <button type="submit" onClick={onSignIn}>
+              Sign In
+            </button>
+          </>
+        ) : (
+          <>
+            <h1>Register</h1>
+            <input
+              defaultValue={email}
+              ref={registerEmailRef}
+              type="email"
+              placeholder="Email"
+            />
+            <input
+              ref={registerPasswordRef}
+              type="password"
+              placeholder="Password"
+            />
+            <button type="submit" onClick={onRegister}>
+              Register
+            </button>
+          </>
+        )}
 
-export default SignInScreen
+        {/* <h4> */}
+        {/* <span className="SignInScreen_gray">New to Netflix?</span> */}
+        {/* <span className="SignInScreen_link" onClick={register}>Sign Up now.</span> */}
+        {/* </h4> */}
+      </form>
+    </div>
+  );
+};
+
+export default SignInScreen;
