a0720c15c2953944b67386c3b9e752afadb730f9
h0uss3m <hossem.benmustapha@gmail.com>
2021-03-18 17:58:05 +0100

    fix remove btn

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,42 +1,46 @@
-import React from "react";
-import "./ListItem.css";
-import { useDispatch } from "react-redux";
-import { removeFavorite } from "../features/favoriteSlice";
-import truncate from "../services";
+import React from 'react'
+import './ListItem.css'
+import { useDispatch } from 'react-redux'
+import { removeFavorite } from '../features/favoriteSlice'
+import truncate from '../services'
 
 const ListItem = ({ movie }) => {
-  const dispatch = useDispatch();
-  const base_url = "https://image.tmdb.org/t/p/original/";
+  const dispatch = useDispatch()
+  const base_url = 'https://image.tmdb.org/t/p/original/'
 
   const removeFromList = (movieItem) => {
-    dispatch(removeFavorite(movieItem));
-  };
+    window.confirm(
+      `Do you really want to remove ${
+        movieItem.original_title || movieItem.original_name
+      }`
+    )
+      ? dispatch(removeFavorite(movieItem))
+      : console.log()
+  }
 
   return (
-    <div className="ListItemScreen_body">
-      <div className="ListItemScreen_info">
+    <div className='ListItemScreen_body'>
+      <div className='ListItemScreen_info'>
         <img
-          className="ListItemScreen_img"
+          className='ListItemScreen_img'
           src={`${base_url}${movie.poster_path}`}
           alt={movie.name}
         />
       </div>
-      <div className="ListItem_description">
-        <h2 className="ListItemScreen_title">
+      <div className='ListItem_description'>
+        <h2 className='ListItemScreen_title'>
           {movie.original_title || movie.original_name}
         </h2>
         <p>{truncate(movie.overview, 450)}</p>
-        <div className="ListItem_delete">
-          <span
-            className="ListItem_delete_button"
-            onClick={() => removeFromList(movie)}
-          >
-            Remove from list
-          </span>
+        <div
+          onClick={() => removeFromList(movie)}
+          className='ListItem_delete ListItem_delete_button'
+        >
+          Remove from list
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ListItem;
+export default ListItem
